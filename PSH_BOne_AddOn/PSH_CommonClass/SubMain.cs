28f833847bddac7bd05dcfd965207d139cc8f223
Song Myounggyu <mg-song@poongsan.co.kr>
2020-05-07 17:58:44 +0900

    Merge Test Rollback

--- PSH_BOne_AddOn/PSH_CommonClass/SubMain.cs
+++ PSH_BOne_AddOn/PSH_CommonClass/SubMain.cs
@@ -6,7 +6,7 @@ namespace PSH_BOne_AddOn
 	static class SubMain
     {
         /// <summary>
-        /// 최초실행 Method(Merge Test용 주석)
+        /// 최초실행 Method
         /// </summary>
         static void Main()
         {

dcca4664a2934f750fbf65b7c6e3a86a9b96ad84
YS Hwang <yswhang@poongsan.co.kr>
2020-06-03 19:53:04 +0900

    운영이관요청서(2020년06월03일)

--- PSH_BOne_AddOn/PSH_CommonClass/PSH_MainClass.cs
+++ PSH_BOne_AddOn/PSH_CommonClass/PSH_MainClass.cs
@@ -192,6 +192,7 @@ namespace PSH_BOne_AddOn
                 }
                 else
                 {
+
                     PSH_Globals.SP_ODBC_Name = "PSHERP_TEST";
                 }
                 PSH_Globals.SP_ODBC_IP = ServerName;

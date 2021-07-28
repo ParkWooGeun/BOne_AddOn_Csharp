963940f7598dd686537fffb698cdeefd6d7702c3
YS Hwang <yswhang@poongsan.co.kr>
2021-07-28 14:05:51 +0900

    C#Migration
    - PS_PP010, PS_PP020 신동 -> 소재 변경

--- PSH_BOne_AddOn/FormClass/PP/PS_PP010.cs
+++ PSH_BOne_AddOn/FormClass/PP/PS_PP010.cs
@@ -178,7 +178,7 @@ namespace PSH_BOne_AddOn
                 oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("10", "영업");
                 oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("20", "정비");
                 oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("30", "멀티");
-                oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("40", "신동");
+                oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("40", "소재");
                 oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("50", "R/D");
                 oForm.Items.Item("WorkGbn").Specific.ValidValues.Add("60", "견본");
                 oForm.Items.Item("WorkGbn").Specific.Select(0, SAPbouiCOM.BoSearchKey.psk_Index);
@@ -187,7 +187,7 @@ namespace PSH_BOne_AddOn
                 oMat01.Columns.Item("WorkGbn").ValidValues.Add("10", "영업");
                 oMat01.Columns.Item("WorkGbn").ValidValues.Add("20", "정비");
                 oMat01.Columns.Item("WorkGbn").ValidValues.Add("30", "멀티");
-                oMat01.Columns.Item("WorkGbn").ValidValues.Add("40", "신동");
+                oMat01.Columns.Item("WorkGbn").ValidValues.Add("40", "소재");
                 oMat01.Columns.Item("WorkGbn").ValidValues.Add("50", "R/D");
                 oMat01.Columns.Item("WorkGbn").ValidValues.Add("60", "견본");
 

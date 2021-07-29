4ba11a3d337142f6f9aa14583618a86755ca12ed
MG Song <mg-song@poongsan.co.kr>
2021-07-29 15:35:48 +0900

    C#Migration 구현중
     - FORM_UNLOAD 이벤트 : 아이템 메모리 해제 코드 위치 변경(Before_Action == true -> Before_Action == false)

--- PSH_BOne_AddOn/FormClass/PY/PH_PY119.cs
+++ PSH_BOne_AddOn/FormClass/PY/PH_PY119.cs
@@ -709,13 +709,13 @@ namespace PSH_BOne_AddOn
             {
                 if (pVal.Before_Action == true)
                 {
+                }
+                else if (pVal.Before_Action == false)
+                {
                     SubMain.Remove_Forms(oFormUniqueID);
                     System.Runtime.InteropServices.Marshal.ReleaseComObject(oForm);
                     System.Runtime.InteropServices.Marshal.ReleaseComObject(oMat1);
                     System.Runtime.InteropServices.Marshal.ReleaseComObject(oDS_PH_PY119B);
-                }
-                else if (pVal.Before_Action == false)
-                {   
                 }
             }
             catch (Exception ex)

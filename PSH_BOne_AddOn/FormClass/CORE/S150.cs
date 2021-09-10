7deb6e4762a5728955c006fda5ff2005a4762b55
MG Song <mg-song@poongsan.co.kr>
2021-09-10 11:20:16 +0900

    C# Migration 구현중
     - S150(품목마스터) : 코드사용여부 콤보박스 pane 설정

--- PSH_BOne_AddOn/FormClass/CORE/S150.cs
+++ PSH_BOne_AddOn/FormClass/CORE/S150.cs
@@ -62,6 +62,8 @@ namespace PSH_BOne_AddOn.Core
                 oItem.Height = oForm.Items.Item("10002052").Height;
                 oItem.Width = 80;
                 oItem.Specific.Caption = "코드사용여부";
+                oItem.FromPane = 6;
+                oItem.ToPane = 6;
 
                 oItem_ItmMsort = oForm.Items.Add("ItmMsort", SAPbouiCOM.BoFormItemTypes.it_EDIT);
                 oItem_ItmMsort.Left = 250;
@@ -99,6 +101,8 @@ namespace PSH_BOne_AddOn.Core
                 oCombo.Specific.ValidValues.Add("", "-");
                 oCombo.Specific.ValidValues.Add("Y", "사용");
                 oCombo.Specific.ValidValues.Add("N", "미사용");
+                oCombo.FromPane = 6;
+                oCombo.ToPane = 6;
             }
             catch (Exception ex)
             {

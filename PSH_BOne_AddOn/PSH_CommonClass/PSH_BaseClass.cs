7334f3374cef0f993859b59224af127190f49404
YS Hwang <yswhang@poongsan.co.kr>
2021-09-15 13:52:59 +0900

    Issue120-2
    S150 수정 , PSH_BaseClass, PSH_MainClass 수정

--- PSH_BOne_AddOn/PSH_CommonClass/PSH_BaseClass.cs
+++ PSH_BOne_AddOn/PSH_CommonClass/PSH_BaseClass.cs
@@ -7,27 +7,15 @@
     {
         public SAPbouiCOM.Form oForm;
 
-        public virtual void LoadForm()
-        {
-        }
-
         public virtual void LoadForm(string oFormDocEntry)
         {
         }
-        
-        public virtual void VirtualFormItemEnabled()
+
+        public virtual void Raise_FormItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pVal, ref bool BubbleEvent)
         {
         }
 
-        public virtual void LoadForm(string FromDate, string ToDate, string BPLID, string StdDt, string TabID)
-        {
-        }
-
-        public virtual void Raise_FormItemEvent(string FormUID, ref SAPbouiCOM.ItemEvent pval, ref bool BubbleEvent)
-        {
-        }
-
-        public virtual void Raise_FormMenuEvent(string FormUID, ref SAPbouiCOM.MenuEvent pval, ref bool BubbleEvent)
+        public virtual void Raise_FormMenuEvent(string FormUID, ref SAPbouiCOM.MenuEvent pVal, ref bool BubbleEvent)
         {
         }
 
@@ -35,9 +23,8 @@
         {
         }
 
-        public virtual void Raise_RightClickEvent(string FormUID, ref SAPbouiCOM.ContextMenuInfo pval, ref bool BubbleEvent)
+        public virtual void Raise_RightClickEvent(string FormUID, ref SAPbouiCOM.ContextMenuInfo pVal, ref bool BubbleEvent)
         {
         }
-
     }
 }

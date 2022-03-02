61a9e899b037fff2e34f5957ae5d23b9067f0050
ChangSikJung <csjung1963@gmail.com>
2022-03-02 10:17:01 +0900

    C#Migration_HR
    - HR200 진행

---
+++ PSH_BOne_AddOn/FormClass/HR/PS_HR200.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSH_BOne_AddOn.FormClass.HR
+{
+    class PS_HR200
+    {
+    }
+}

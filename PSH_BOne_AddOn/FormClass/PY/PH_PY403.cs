90f50b14b79bad6706ca98abb782487cb0ca384b
ChangSikJung <csjung1963@gmail.com>
2022-12-14 10:16:36 +0900

    정산수정 - PH_PY402 화면순서변경

--- PSH_BOne_AddOn/FormClass/PY/PH_PY403.cs
+++ PSH_BOne_AddOn/FormClass/PY/PH_PY403.cs
@@ -660,9 +660,6 @@ namespace PSH_BOne_AddOn
             {
                 PSH_Globals.SBO_Application.StatusBar.SetText("PH_PY710_Print_Report01_Error : " + ex.Message, BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
             }
-            finally
-            {
-            }
         }
 
         /// <summary>

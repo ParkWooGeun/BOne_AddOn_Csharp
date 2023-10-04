e0c704e6c324cf4f582f8a31ecef9324d6be2556
CPARKWOOGEUN\CParkWooGeun <dnrms960@naver.com>
2023-10-04 14:46:38 +0900

    issuse183-1 가공품출고 수정

--- PSH_BOne_AddOn/FormClass/MM/PS_MM130.cs
+++ PSH_BOne_AddOn/FormClass/MM/PS_MM130.cs
@@ -680,11 +680,14 @@ namespace PSH_BOne_AddOn
 						errMessage = "외주 마지막공정(외주공정구간)을 선택하셔야 합니다.";
 						throw new Exception();
 					}
-					else if (Convert.ToDouble(oMat.Columns.Item("OutWt").Cells.Item(i).Specific.Value.ToString().Trim()) > Convert.ToDouble(oMat.Columns.Item("Weight").Cells.Item(i).Specific.Value.ToString().Trim()))
+					else if (oForm.Items.Item("BPLId").Specific.Value.ToString().Trim() == "2") //창원은 중량에 상관없이 나감
 					{
-						oMat.Columns.Item("OutWt").Cells.Item(i).Click(SAPbouiCOM.BoCellClickType.ct_Regular);
-						errMessage = "반출수(중)량은 기준 중량을 초과할수 없습니다.";
-						throw new Exception();
+						if (Convert.ToDouble(oMat.Columns.Item("OutWt").Cells.Item(i).Specific.Value.ToString().Trim()) > Convert.ToDouble(oMat.Columns.Item("Weight").Cells.Item(i).Specific.Value.ToString().Trim()))
+						{
+							oMat.Columns.Item("OutWt").Cells.Item(i).Click(SAPbouiCOM.BoCellClickType.ct_Regular);
+							errMessage = "반출수(중)량은 기준 중량을 초과할수 없습니다.";
+							throw new Exception();
+						}
 					}
 				}
 				oDS_PS_MM130L.RemoveRecord(oDS_PS_MM130L.Size - 1);
